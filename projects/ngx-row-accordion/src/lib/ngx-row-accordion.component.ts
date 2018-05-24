65a54a4befc0bf449917f3102f96e04ec3654ba8
Maxime Robert <maxime.robert1992@gmail.com>
2018-05-24 14:42:34 +0100

    chore(demo): import ngx-row-accordion component

--- projects/ngx-row-accordion/src/lib/ngx-row-accordion.component.ts
+++ projects/ngx-row-accordion/src/lib/ngx-row-accordion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngx-row-accordion',
+  selector: 'ngx-row-accordion',
   template: `
     <p>
       ngx-row-accordion works!
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxRowAccordionModule } from 'ngx-row-accordion';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgxRowAccordionModule],
   providers: [],
   bootstrap: [AppComponent],
 })
