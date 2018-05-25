d3bae216f9dcf5ebdf793b228b588ff57efb9a14
Maxime Robert <maxime.robert1992@gmail.com>
2018-05-25 14:52:36 +0100

    feat(lib): service to handle accordions by group

--- projects/ngx-row-accordion/src/lib/ngx-row-accordion.component.ts
+++ projects/ngx-row-accordion/src/lib/ngx-row-accordion.component.ts
@@ -1,18 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { NgxRowAccordionService } from './ngx-row-accordion.service';
+import { tap, takeUntil } from 'rxjs/operators';
+import { merge, Subject } from 'rxjs';
 
 @Component({
   selector: 'ngx-row-accordion',
   templateUrl: './ngx-row-accordion.component.html',
   styleUrls: ['./ngx-row-accordion.component.scss'],
 })
-export class NgxRowAccordionComponent implements OnInit {
+export class NgxRowAccordionComponent implements OnInit, OnDestroy {
   @Input() title: string;
+
+  // name of the group this accordions belongs to
+  @Input() group: string;
 
   isFolded: boolean = false;
 
-  constructor() {}
+  private onDestroy$: Subject<void> = new Subject();
 
-  ngOnInit() {}
+  constructor(private ngxRowAccordionService: NgxRowAccordionService) {}
+
+  ngOnInit() {
+    if (!this.group) {
+      throw new Error('[ngx-row-accordion] you should always pass a group when creating a row-accordion');
+    }
+
+    const index = this.ngxRowAccordionService.addComponentToGroup(this, this.group);
+
+    merge(
+      this.ngxRowAccordionService.onAddInGroup(this.group).pipe(tap(x => this.fold())),
+      this.ngxRowAccordionService.onRemoveInGroup(this.group).pipe(
+        tap(removedIndex => {
+          if (index === removedIndex - 1) {
+            this.unfold();
+          }
+        })
+      )
+    )
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.complete();
+
+    this.ngxRowAccordionService.removeComponentFromGroup(this);
+  }
 
   fold() {
     this.isFolded = true;
--- projects/ngx-row-accordion/src/lib/ngx-row-accordion.service.ts
+++ projects/ngx-row-accordion/src/lib/ngx-row-accordion.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { filter, mapTo, map } from 'rxjs/operators';
+import { NgxRowAccordionComponent } from './ngx-row-accordion.component';
+import { mapChildrenIntoArray } from '@angular/router/src/url_tree';
+
+type Group = Map<NgxRowAccordionComponent, undefined>;
 
 @Injectable({
   providedIn: 'root',
 })
 export class NgxRowAccordionService {
+  private groups: Map<string, Group> = new Map();
+  private componentToGroup: Map<NgxRowAccordionComponent, string> = new Map();
+  private _onAddInGroup$: Subject<string> = new Subject();
+  private _onRemoveInGroup$: Subject<{ name: string; index: number }> = new Subject();
+
   constructor() {}
+
+  private addGroupIfDoesNotExists(groupName: string): void {
+    if (!this.groups.has(groupName)) {
+      this.groups.set(groupName, new Map());
+    }
+  }
+
+  addComponentToGroup(componentRef: NgxRowAccordionComponent, groupName: string): number {
+    this.addGroupIfDoesNotExists(groupName);
+
+    const group: Group = this.groups.get(groupName);
+
+    if (group.has(componentRef)) {
+      throw new Error('A row-accordion should be registered only once');
+    }
+
+    group.set(componentRef, undefined);
+
+    this.componentToGroup.set(componentRef, groupName);
+
+    this._onAddInGroup$.next(groupName);
+
+    return group.size - 1;
+  }
+
+  removeComponentFromGroup(componentRef: NgxRowAccordionComponent) {
+    const groupName: string = this.componentToGroup.get(componentRef);
+
+    this.groups.get(groupName).delete(componentRef);
+
+    this._onRemoveInGroup$.next({ name: groupName, index: this.groups.get(groupName).size });
+
+    if (this.groups.get(groupName).size === 0) {
+      this.groups.delete(groupName);
+    }
+  }
+
+  onAddInGroup(groupName: string): Observable<void> {
+    return this._onAddInGroup$.pipe(filter(gName => gName === groupName), mapTo(undefined));
+  }
+
+  onRemoveInGroup(groupName: string): Observable<number> {
+    return this._onRemoveInGroup$.pipe(filter(({ name, index }) => name === groupName), map(({ index }) => index));
+  }
 }
