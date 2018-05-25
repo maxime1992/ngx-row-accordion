e62e5aa316c649cc7c169e93bed870dbdd7aa8c0
Maxime Robert <maxime.robert1992@gmail.com>
2018-05-25 10:49:01 +0100

    feat(lib): basic scaffolding of the accordion

--- projects/ngx-row-accordion/src/lib/ngx-row-accordion.component.ts
+++ projects/ngx-row-accordion/src/lib/ngx-row-accordion.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ngx-row-accordion',
   templateUrl: './ngx-row-accordion.component.html',
-  styles: [],
+  styleUrls: ['./ngx-row-accordion.component.scss'],
 })
 export class NgxRowAccordionComponent implements OnInit {
+  @Input() title: string;
+
+  isFolded: boolean = false;
+
   constructor() {}
 
   ngOnInit() {}
+
+  fold() {
+    this.isFolded = true;
+  }
+
+  unfold() {
+    this.isFolded = false;
+  }
+
+  toggle() {
+    this.isFolded = !this.isFolded;
+  }
 }
--- projects/ngx-row-accordion/src/lib/ngx-row-accordion.module.ts
+++ projects/ngx-row-accordion/src/lib/ngx-row-accordion.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { NgxRowAccordionComponent } from './ngx-row-accordion.component';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+// @todo RouterModule should be passed within forRoot to avoid creating new instances
+// this will prevent unexpected behavior with interceptors on main app
 @NgModule({
-  imports: [FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, RouterModule],
   declarations: [NgxRowAccordionComponent],
   exports: [NgxRowAccordionComponent],
 })
